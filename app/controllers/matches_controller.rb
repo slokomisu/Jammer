97ce58a098c22688df329f82e50ca84cb7c17f01
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 13:34:14 -0500

    For real this time

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,11 @@
 class MatchesController < ApplicationController
   before_action :authenticate_user!
   def index
-    @swipes = Swipe.where(swipee_id: current_user.id, liked: true)
+    swipes = Swipe.where(swipee_id: current_user.id, liked: true)
+    @matches = []
+    swipes.each do |swipe|
+      @matches.push(User.find(swipe.user_id))
+    end
+    @matches
   end
 end
