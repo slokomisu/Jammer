94695426e99d13f33b02f5e6df8598a8b17749ef
Tanner Stephens <Tanner.Stephens@cerner.com>
2018-07-19 11:43:55 -0500

    Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'swipes#index'
   devise_for :users
-  get 'swipes/index'
-  get 'matches/index'
+  get 'swipes', to: 'swipes#index'
+  get 'matches', to: 'matches/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
