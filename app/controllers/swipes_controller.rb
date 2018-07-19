47702f44b14618246ff4ab631ef571d8109c81a0
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 15:54:35 -0500

    Fix a bunch of stuff

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -10,10 +10,12 @@ class SwipesController < ApplicationController
 
   def swipe
     swipe = Swipe.create(user_id: current_user.id, swipee_id: params[:swipee_id], liked: params[:liked])
-    potential_match = Swipe.where(user_id: swipee_id, swipee_id: current_user.id, liked: true)
+    potential_match = Swipe.where(user_id: params[:swipee_id], swipee_id: current_user.id, liked: true)
     if !potential_match.empty?
       match = User.find(params[:swipee_id])
       flash[:notice] = "You have matched with #{match.name}!"
+      flash.keep
+      redirect_to swipes_path
     end
   end
 end
