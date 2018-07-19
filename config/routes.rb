a12b80e8543a0fb485670e47b058a991b7d2ed12
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 13:20:07 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
-  get '/swipes' => 'swipes#index'
-  post '/swipes/:user_id/:swipee_id' => 'swipes#swipe'
-  get '/matches' => 'matches/index'
+  get 'swipes' => 'swipes#index'
+  post 'swipes/:user_id/:swipee_id' => 'swipes#swipe'
+  get 'matches' => 'matches/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
