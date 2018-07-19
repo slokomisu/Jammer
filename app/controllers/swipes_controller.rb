c601cf5e6a24dfb0767ee059049a25a542317074
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 14:26:40 -0500

    Add swipe logic

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -5,6 +5,11 @@ class SwipesController < ApplicationController
   end
 
   def swipe
-
+    swipe = Swipe.create(user_id: current_user.id, swipee_id: params[:swipee_id], liked: params[:liked])
+    potential_match = Swipe.where(user_id: swipee_id, swipee_id: current_user.id, liked: true)
+    if !potential_match.empty?
+      match = User.find(params[:swipee_id])
+      flash[:notice] = "You have matched with #{match.name}!"
+    end
   end
 end
