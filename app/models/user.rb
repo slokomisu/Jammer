6b3bd5a5fb115c6f5ea15534f7ce2c94d1ff6e8f
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 14:15:11 -0500

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :swipes
   has_one_attached :clip
+  validates :name, presence: true
+  validates :instrument, presence: true
 
   def matches
     matches = []
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
   get 'swipes' => 'swipes#index'
-  post 'swipes/:user_id/:swipee_id' => 'swipes#swipe'
+  post 'swipes/:swipee_id' => 'swipes#swipe'
   get 'matches' => 'matches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
