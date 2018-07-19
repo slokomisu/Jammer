ae5bb43ee9705272d47a79e04045286b4208a178
Noah Guillory <Noah.Guillory@cerner.com>
2018-07-19 11:45:03 -0500

    Add swipe action and route

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,4 +1,9 @@
 class SwipesController < ApplicationController
   def index
+
+  end
+
+  def swipe
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'swipes#index'
   devise_for :users
   get 'swipes', to: 'swipes#index'
+  post 'swipes', to: 'swipes#swipe'
   get 'matches', to: 'matches/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
